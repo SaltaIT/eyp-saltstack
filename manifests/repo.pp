ab1a44db666d6e2c0e415ad2110432394e21ec8b
Jordi Prats <jprats@systemadmin.es>
2016-10-27 16:32:37 +0200

    exec path i which wget

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,5 +1,14 @@
 class saltstack::repo($srcdir = '/usr/local/src') inherits saltstack::params {
 
+  Exec {
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  exec { 'which wget eyp-saltstack':
+    command => 'which wget',
+    unless  => 'which wget',
+  }
+
   exec { "mkdir p eyp-saltstack ${srcdir}":
     command => "mkdir -p ${srcdir}",
     creates => $srcdir,
@@ -8,7 +17,7 @@ class saltstack::repo($srcdir = '/usr/local/src') inherits saltstack::params {
   exec { 'wget saltstack repo':
     command => "wget ${saltstack::params::saltstack_repo_url} -O ${srcdir}/saltstack_repo.${puppet::params::package_provider}",
     creates => "${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
-    require => Exec["mkdir p eyp-saltstack ${srcdir}"],
+    require => Exec[ "mkdir p eyp-saltstack ${srcdir}", 'which wget eyp-saltstack' ],
   }
 
   package { $puppet::params::puppetlabs_package:

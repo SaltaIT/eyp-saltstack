12ba7216777cb75f26a37201988fb74361c55e9f
Jordi Prats <jprats@systemadmin.es>
2016-10-27 18:23:14 +0200

    minion::

--- manifests/minion/service.pp
+++ manifests/minion/service.pp
@@ -1,18 +1,18 @@
 class saltstack::minion::service inherits saltstack::minion {
   #
-  validate_bool($saltstack::manage_docker_service)
-  validate_bool($saltstack::manage_service)
-  validate_bool($saltstack::service_enable)
+  validate_bool($saltstack::minion::manage_docker_service)
+  validate_bool($saltstack::minion::manage_service)
+  validate_bool($saltstack::minion::service_enable)
 
-  validate_re($saltstack::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${saltstack::service_ensure}")
+  validate_re($saltstack::minion::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${saltstack::minion::service_ensure}")
 
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
   if( $is_docker_container==false or
-      $saltstack::manage_docker_service)
+      $saltstack::minion::manage_docker_service)
   {
-    if($saltstack::manage_service)
+    if($saltstack::minion::manage_service)
     {
       service { $saltstack::params::minion_service_name:
         ensure => $saltstack::minion::service_enable,

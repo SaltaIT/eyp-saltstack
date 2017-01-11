1c494d9f3cb58fa42ccc58d4d52991943279371b
Jordi Prats <jprats@systemadmin.es>
2017-01-11 11:47:59 +0100

    config master

--- manifests/master/config.pp
+++ manifests/master/config.pp
@@ -1,4 +1,11 @@
 #
 class saltstack::master::config inherits saltstack::master {
   #
+  file { '/etc/salt/master':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template("${module_name}/master/master.erb"),
+  }
 }

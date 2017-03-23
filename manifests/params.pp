d1781a733c2af78f9a4178bb079fcfc712b7e942
Jordi Prats <jprats@systemadmin.es>
2017-03-23 18:17:19 +0100

    uport ubuntu

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,14 +17,20 @@ class saltstack::params {
       {
         /^5.*$/:
         {
+          $saltstack_repo_url_key=undef
+          $saltstack_repo_url_key_source=undef
           $saltstack_repo_url='https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el5.noarch.rpm'
         }
         /^6.*$/:
         {
+          $saltstack_repo_url_key=undef
+          $saltstack_repo_url_key_source=undef
           $saltstack_repo_url='https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el6.noarch.rpm'
         }
         /^7.*$/:
         {
+          $saltstack_repo_url_key=undef
+          $saltstack_repo_url_key_source=undef
           $saltstack_repo_url='https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el7.noarch.rpm'
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
@@ -40,7 +46,15 @@ class saltstack::params {
           {
             /^14.*$/:
             {
-              fail('currently unsupported - fuck off')
+              $saltstack_repo_url_key='C7A3D3EE96D9220BAE9420246DF2C88E747F3421'
+              $saltstack_repo_url_key_source='https://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest/SALTSTACK-GPG-KEY.pub'
+              $saltstack_repo_url='http://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest'
+            }
+            /^16.*$/:
+            {
+              $saltstack_repo_url_key='C7A3D3EE96D9220BAE9420246DF2C88E747F3421'
+              $saltstack_repo_url_key_source='https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub'
+              $saltstack_repo_url='http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest'
             }
             default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
           }
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -6,28 +6,54 @@ class saltstack::repo($srcdir = '/usr/local/src') inherits saltstack::params {
 
   #TODO:
   # ubuntu: https://repo.saltstack.com/#ubuntu
+  case $::osfamily
+  {
+    'redhat':
+    {
+      exec { 'which wget eyp-saltstack':
+        command => 'which wget',
+        unless  => 'which wget',
+      }
 
-  exec { 'which wget eyp-saltstack':
-    command => 'which wget',
-    unless  => 'which wget',
+      exec { "mkdir p eyp-saltstack ${srcdir}":
+        command => "mkdir -p ${srcdir}",
+        creates => $srcdir,
+      }
+
+      exec { 'wget saltstack repo':
+        command => "wget ${saltstack::params::saltstack_repo_url} -O ${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
+        creates => "${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
+        require => Exec[ "mkdir p eyp-saltstack ${srcdir}", 'which wget eyp-saltstack' ],
+      }
+
+      package { $saltstack::params::saltstack_repo_name:
+        ensure   => 'installed',
+        provider => $saltstack::params::package_provider,
+        source   => "${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
+        require  => Exec['wget saltstack repo'],
+      }
+    }
+    'Debian':
+    {
+      apt::key { 'SALTSTACK-GPG-KEY':
+        key        => $saltstack::params::saltstack_repo_url_key,
+        key_source => $saltstack::params::saltstack_repo_url_key_source,
+      }
+
+      # deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main
+      apt::source { 'saltstack':
+        location => $saltstack::params::saltstack_repo_url,
+        release  => $::lsbdistcodename,
+        repos    => "${::lsbdistcodename}/main",
+        require  => Apt::Key['SALTSTACK-GPG-KEY'],
+      }
+    }
+    default:
+    {
+      fail('unsupported')
+    }
   }
 
-  exec { "mkdir p eyp-saltstack ${srcdir}":
-    command => "mkdir -p ${srcdir}",
-    creates => $srcdir,
-  }
 
-  exec { 'wget saltstack repo':
-    command => "wget ${saltstack::params::saltstack_repo_url} -O ${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
-    creates => "${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
-    require => Exec[ "mkdir p eyp-saltstack ${srcdir}", 'which wget eyp-saltstack' ],
-  }
-
-  package { $saltstack::params::saltstack_repo_name:
-    ensure   => 'installed',
-    provider => $saltstack::params::package_provider,
-    source   => "${srcdir}/saltstack_repo.${saltstack::params::package_provider}",
-    require  => Exec['wget saltstack repo'],
-  }
 
 }

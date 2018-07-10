bfdea3c8a86608480146d35632f0beec6f9cea02
Jordi Prats <jordi.prats@gmail.com>
2018-07-10 13:43:01 +0200

    selfsigned cert

--- manifests/api/config.pp
+++ manifests/api/config.pp
@@ -1,5 +1,15 @@
 class saltstack::api::config inherits saltstack::api {
 
+  if($saltstack::generate_selfsigned_cert)
+  {
+    exec { 'selfsigned cert API':
+      command => 'salt-call --local tls.create_self_signed_cert',
+      path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+      creates => '/etc/pki/tls/certs/localhost.crt',
+      before  => File['/etc/salt/master.d/salt-api.conf'],
+    }
+  }
+
   file { '/etc/salt/master.d/salt-api.conf':
     ensure  => 'present',
     owner   => 'root',
@@ -7,4 +17,5 @@ class saltstack::api::config inherits saltstack::api {
     mode    => '0664',
     content => template("${module_name}/api/api.erb"),
   }
+
 }

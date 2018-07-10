e76739e59fa8c3ab02c96fed32f0dbffb3d23c2d
Jordi Prats <jordi.prats@gmail.com>
2018-07-10 18:00:29 +0200

    lint

--- manifests/api/install.pp
+++ manifests/api/install.pp
@@ -27,7 +27,7 @@ class saltstack::api::install inherits saltstack::api {
     if($saltstack::params::api_pip_dependencies!=undef)
     {
       include ::python
-      
+
       pythonpip { $saltstack::params::api_pip_dependencies:
         ensure => $pip_ensure,
         before => Package[$saltstack::params::api_package_name],

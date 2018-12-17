cd89096e9adf15ab570cef6f1cd83f70f8dabd53
Jordi Prats <jordi.prats@gmail.com>
2018-12-17 12:45:09 +0100

    lint

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -32,11 +32,11 @@ class saltstack::repo (
         #        https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/base/RPM-GPG-KEY-CentOS-7
 
         yumrepo { 'saltstack-repo':
-          baseurl => "https://repo.saltstack.com/yum/redhat/$releasever/$basearch/archive/${version}.${version_minor}",
-          descr => "SaltStack repo for Red Hat Enterprise Linux - ${version}.${version_minor}",
-          enabled => '1',
+          baseurl  => "https://repo.saltstack.com/yum/redhat/\$releasever/\$basearch/archive/${version}.${version_minor}",
+          descr    => "SaltStack repo for Red Hat Enterprise Linux - ${version}.${version_minor}",
+          enabled  => '1',
           gpgcheck => '1',
-          gpgkey => 'https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/SALTSTACK-GPG-KEY.pub https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/base/RPM-GPG-KEY-CentOS-7',
+          gpgkey   => 'https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/SALTSTACK-GPG-KEY.pub https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/base/RPM-GPG-KEY-CentOS-7',
         }
       }
       else

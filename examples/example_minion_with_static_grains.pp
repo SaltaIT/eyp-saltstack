b356a4e18362b85b04043d1027b8a4577ba927ca
Jordi Prats <jordi.prats@gmail.com>
2019-02-21 17:18:18 +0100

    deno grains

---
+++ examples/example_minion_with_static_grains.pp
@@ -0,0 +1,46 @@
+class { 'saltstack::repo':
+  version       => '2018.3',
+  version_minor => '2',
+}
+
+class { 'saltstack::minion':
+  master => '127.0.0.1'
+}
+
+saltstack::minion::grain { 'demo1':
+  value => 'demo text',
+}
+
+saltstack::minion::grain { 'demo2':
+  value => [ 'array', 'data' ],
+}
+
+class { 'saltstack::master': }
+
+saltstack::master::fileroot { 'base':
+  files => [ '/srv/salt-data/base' ],
+}
+
+saltstack::master::pillar { 'base':
+  files => [ '/srv/salt-data/pillar' ],
+}
+
+class { 'saltstack::cloud':
+  install_windows_dependencies => false,
+}
+
+class { 'saltstack::api': }
+
+class { 'saltstack::syndic': }
+
+saltstack::master::key { $::fqdn:
+  status => 'accepted'
+}
+
+saltstack::master::acl { 'saltuser':
+  match => [ '.*', '@runner' ],
+}
+
+saltstack::master::acl { 'saltuser2':
+  match => [ '.*', '@runner' ],
+}

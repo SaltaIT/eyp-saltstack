23a4c6ea1b9992a09f6d201b3d7f00847e5da859
Jordi Prats <jordi.prats@gmail.com>
2020-03-30 12:14:46 +0200

    cleanup validate

--- manifests/master/service.pp
+++ manifests/master/service.pp
@@ -1,10 +1,4 @@
 class saltstack::master::service inherits saltstack::master {
-  #
-  validate_bool($saltstack::master::manage_docker_service)
-  validate_bool($saltstack::master::manage_service)
-  validate_bool($saltstack::master::service_enable)
-
-  validate_re($saltstack::master::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${saltstack::master::service_ensure}")
 
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
--- manifests/minion/service.pp
+++ manifests/minion/service.pp
@@ -1,9 +1,4 @@
 class saltstack::minion::service inherits saltstack::minion {
-  validate_bool($saltstack::minion::manage_docker_service)
-  validate_bool($saltstack::minion::manage_service)
-  validate_bool($saltstack::minion::service_enable)
-
-  validate_re($saltstack::minion::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${saltstack::minion::service_ensure}")
 
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
